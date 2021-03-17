04367ea91135be1f4b15a28281f812a584df396c
jiangtj <116749895@qq.com>
2021-03-17 17:02:38 +0800

    ...

--- test/lib/bundle/bundle-js.js
+++ test/lib/bundle/bundle-js.js
@@ -4,6 +4,7 @@ require('chai').should();
 const Hexo = require('hexo');
 const Injector = require('../../../lib/injector');
 const jsGenerator = require('../../../lib/bundle/js-generator');
+const { resolve } = require('path');
 
 describe('JS Bundler', () => {
   it('basic', () => {
@@ -19,4 +20,17 @@ describe('JS Bundler', () => {
       result.should.eql('var a=1,b=1;');
     });
   });
+
+  it('v0.2 scheme', () => {
+    const hexo = new Hexo();
+    const injector = new Injector(hexo);
+    require('../../../lib/bundle/js-bundle')(hexo, injector);
+    injector.register('js', { path: resolve(__dirname, 'test.js') });
+    injector.register('js', { text: () => 'var b=1;' });
+    const exec = jsGenerator(injector);
+    return exec().then(result => {
+      result.should.eql('var a=1,b=1;');
+    });
+  });
+
 });
